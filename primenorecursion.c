699738907301b4710e62652bbdf1b1de9f203553
adityabolt <61606845+adityabolt@users.noreply.github.com>
2020-05-18 20:44:59 +0530

    Create primenorecursion.c

---
+++ primenorecursion.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int isPrime(int n,int k);
+int main()
+{
+  int n,sum;
+  printf("Enter a number ");
+  scanf("%d",&n);
+  if(isPrime(n,2)==1)
+    printf("%d is a prime number",n);
+  else
+    printf("%d is not a prime number",n);
+}
+int isPrime(int n,int k)
+{
+  if(n==k)
+    return 1;
+  else if(n%k==0)
+    return 0;
+  return isPrime(n,k+1);
+}
