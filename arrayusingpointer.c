ad63d832808962ca5534b5aa033a16f8bbaea68e
adityabolt <61606845+adityabolt@users.noreply.github.com>
2020-05-18 22:42:03 +0530

    Create arrayusingpointer.c

---
+++ arrayusingpointer.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+ int main()
+ {
+   int arr[3][3],i,j;
+   printf("Input elements in the matrix\n");
+   for(i=0;i<3;i++){
+     for(j=0;j<3;j++){
+       printf("element-[%d],[%d]: ",i,j);
+       scanf("%d",(*(arr+i)+j));
+     }
+   }
+   printf("The matrix is:\n");
+   for(i=0;i<3;i++){
+     for(j=0;j<3;j++){
+       printf("%d\t",*(*(arr+i)+j));
+     }
+     printf("\n");
+   }
+ }
